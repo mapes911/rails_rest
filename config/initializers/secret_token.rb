42b7f5eb5afc9ab9cb5ecc44e05cc45c2fd505c7
vagrant <vagrant@suite101-rails-dev-box.(none)>
2013-02-11 00:08:15 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsRest::Application.config.secret_token = 'ab1bde5fd743cb2e03cbd6f647c8aabfc472f77139b5ee32e89551e9d91a1c1fa053f362ff813acf7444a9e1913313770fe63a9ce56a62096da2c9790d86dad0'
